6be6d4ecbc15aa87a34d85c14d6234ba972c7364
Evgeny Ivanov <busforward@gmail.com>
2018-07-25 13:41:27 +0500

    На проверку

--- js/function.js
+++ js/function.js
@@ -57,32 +57,12 @@ $(document).ready(function() {
 		event.preventDefault();
 	});
 
-	// Scroll to ID // Плавный скролл к элементу при нажатии на ссылку. В ссылке указываем ID элемента
-	// $('#main__menu a[href^="#"]').click( function(){ 
-	// 	var scroll_el = $(this).attr('href'); 
-	// 	if ($(scroll_el).length != 0) {
-	// 	$('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500);
-	// 	}
-	// 	return false;
-	// });
-
-	// Stiky menu // Липкое меню. При прокрутке к элементу #header добавляется класс .stiky который и стилизуем
-    // $(document).ready(function(){
-    //     var HeaderTop = $('#header').offset().top;
-        
-    //     $(window).scroll(function(){
-    //             if( $(window).scrollTop() > HeaderTop ) {
-    //                     $('#header').addClass('stiky');
-    //             } else {
-    //                     $('#header').removeClass('stiky');
-    //             }
-    //     });
-    // });
-
     // Inputmask.js
     // $('[name=tel]').inputmask("+9(999)999 99 99",{ showMaskOnHover: false });
-
-   	gridMatch();
+    fontResize();
+    animation();
+    formSubmit();
+   	
 });
 
 $(window).resize(function(event) {
@@ -91,171 +71,122 @@ $(window).resize(function(event) {
     if (TempApp.resized == windowWidth) { return; }
     TempApp.resized = windowWidth;
 
-	checkOnResize()
+	checkOnResize();
 });
 
 function checkOnResize() {
-   	gridMatch();
+    fontResize();
+   	
 }
 
-function gridMatch() {
-   	$('[data-grid-match] .grid__item').matchHeight({
-   		byRow: true,
-   	});
+function animation() {
+    var speed = 300;
+    var elem = $('.fade');
+    var len = elem.length;
+    var i = 0;
+    function change() {
+        elem.each(function (i, el) {
+            setTimeout(function () {
+                $(el).addClass('animated fadeIn');
+            }, speed * i);
+        });
+    }
+    setTimeout(change, speed);
+
+    setTimeout(function() {
+        $('.footer').addClass('animated fadeIn');
+    }, 200);
+
+    setTimeout(function() {
+        $('.main__phone').addClass('show animated slideInUp');
+    }, 2500);
+
+    setTimeout(function() {
+        // $('.main__phone').addClass('splashOut');
+        $('.main__phone_m').addClass('splashIn');
+    }, 3800);
+
 }
 
 function fontResize() {
     var windowWidth = $(window).width();
-    if (windowWidth < 1440 && windowWidth >= 768) {
-    	var fontSize = windowWidth/19.05;
-    } else if (windowWidth < 768) {
-    	var fontSize = 50;
-    // } else if (windowWidth >= 1770) {
-    // 	var fontSize = 100;
+    if (windowWidth >= 991) {
+        var fontSize = windowWidth/19.05;
+    } 
+
+    if (windowWidth < 991 && windowWidth >= 768) {
+    	var fontSize = windowWidth/17.05;
+    } 
+    if (windowWidth < 768) {
+    	var fontSize = 70;
     }
 	$('body').css('fontSize', fontSize + '%');
 }
 
-// Видео youtube для страницы
-$(function () {
-    if ($(".js_youtube")) {
-        $(".js_youtube").each(function () {
-            // Зная идентификатор видео на YouTube, легко можно найти его миниатюру
-            $(this).css('background-image', 'url(http://i.ytimg.com/vi/' + this.id + '/sddefault.jpg)');
-
-            // Добавляем иконку Play поверх миниатюры, чтобы было похоже на видеоплеер
-            $(this).append($('<img src="img/play.svg" alt="Play" class="video__play">'));
-
-        });
-
-        $('.video__play, .video__prev').on('click', function () {
-            // создаем iframe со включенной опцией autoplay
-            var videoId = $(this).closest('.js_youtube').attr('id');
-            var iframe_url = "https://www.youtube.com/embed/" + videoId + "?autoplay=1&autohide=1";
-            if ($(this).data('params')) iframe_url += '&' + $(this).data('params');
-
-            // Высота и ширина iframe должны быть такими же, как и у родительского блока
-            var iframe = $('<iframe/>', {
-                'frameborder': '0',
-                'src': iframe_url,
-                'width': $(this).width(),
-                'height': $(this).innerHeight()
-            })
-
-            // Заменяем миниатюру HTML5 плеером с YouTube
-            $(this).closest('.video__wrapper').append(iframe);
-
-        });
-    }
-
-});
-
-
-// Деление чисел на разряды Например из строки 10000 получаем 10 000
-// Использование: thousandSeparator(1000) или используем переменную.
-// function thousandSeparator(str) {
-//     var parts = (str + '').split('.'),
-//         main = parts[0],
-//         len = main.length,
-//         output = '',
-//         i = len - 1;
-    
-//     while(i >= 0) {
-//         output = main.charAt(i) + output;
-//         if ((len - i) % 3 === 0 && i > 0) {
-//             output = ' ' + output;
-//         }
-//         --i;
-//     }
-
-//     if (parts.length > 1) {
-//         output += '.' + parts[1];
-//     }
-//     return output;
-// };
-
-
-// Хак для яндекс карт втавленных через iframe
-// Страуктура:
-//<div class="map__wrap" id="map-wrap">
-//  <iframe style="pointer-events: none;" src="https://yandex.ru/map-widget/v1/-/CBqXzGXSOB" width="1083" height="707" frameborder="0" allowfullscreen="true"></iframe>
-//</div>
-// Обязательное свойство в style которое и переключет скрипт
-// document.addEventListener('click', function(e) {
-//     var map = document.querySelector('#map-wrap iframe')
-//     if(e.target.id === 'map-wrap') {
-//         map.style.pointerEvents = 'all'
-//     } else {
-//         map.style.pointerEvents = 'none'
-//     }
-// })
-
 // Простая проверка форм на заполненность и отправка аяксом
-// function formSubmit() {
-//     $("[type=submit]").on('click', function (e){ 
-//         e.preventDefault();
-//         var form = $(this).closest('.form');
-//         var url = form.attr('action');
-//         var form_data = form.serialize();
-//         var field = form.find('[required]');
-//         // console.log(form_data);
+function formSubmit() {
+    $("[type=submit]").on('click', function (e){ 
+        e.preventDefault();
+        var form = $(this).closest('.form');
+        var url = form.attr('action');
+        var form_data = form.serialize();
+        var field = form.find('[required]');
+        empty = 0;
 
-//         empty = 0;
+        field.each(function() {
+            if ($(this).val() == "") {
+                $(this).addClass('invalid');
+                // return false;
+                empty++;
+            } else {
+                $(this).removeClass('invalid');
+                $(this).addClass('valid');
+            }  
+        });
 
-//         field.each(function() {
-//             if ($(this).val() == "") {
-//                 $(this).addClass('invalid');
-//                 // return false;
-//                 empty++;
-//             } else {
-//                 $(this).removeClass('invalid');
-//                 $(this).addClass('valid');
-//             }  
-//         });
+        // console.log(empty);
 
-//         // console.log(empty);
+        if (empty > 0) {
+            return false;
+        } else {        
+            $.ajax({
+                url: url,
+                type: "POST",
+                dataType: "html",
+                data: form_data,
+                success: function (response) {
+                    // $('#success').modal('show');
+                    // console.log('success');
+                    console.log(response);
+                    // console.log(data);
+                    // document.location.href = "success.html";
+                },
+                error: function (response) {
+                    // $('#success').modal('show');
+                    // console.log('error');
+                    console.log(response);
+                }
+            });
+        }
 
-//         if (empty > 0) {
-//             return false;
-//         } else {        
-//             $.ajax({
-//                 url: url,
-//                 type: "POST",
-//                 dataType: "html",
-//                 data: form_data,
-//                 success: function (response) {
-//                     // $('#success').modal('show');
-//                     // console.log('success');
-//                     console.log(response);
-//                     // console.log(data);
-//                     // document.location.href = "success.html";
-//                 },
-//                 error: function (response) {
-//                     // $('#success').modal('show');
-//                     // console.log('error');
-//                     console.log(response);
-//                 }
-//             });
-//         }
+    });
 
-//     });
+    $('[required]').on('blur', function() {
+        if ($(this).val() != '') {
+            $(this).removeClass('invalid');
+        }
+    });
 
-//     $('[required]').on('blur', function() {
-//         if ($(this).val() != '') {
-//             $(this).removeClass('invalid');
-//         }
-//     });
-
-//     $('.form__privacy input').on('change', function(event) {
-//         event.preventDefault();
-//         var btn = $(this).closest('.form').find('.btn');
-//         if ($(this).prop('checked')) {
-//             btn.removeAttr('disabled');
-//             // console.log('checked');
-//         } else {
-//             btn.attr('disabled', true);
-//         }
-//     });
-// }
+    $('.form__privacy input').on('change', function(event) {
+        event.preventDefault();
+        var btn = $(this).closest('.form').find('.btn');
+        if ($(this).prop('checked')) {
+            btn.removeAttr('disabled');
+            // console.log('checked');
+        } else {
+            btn.attr('disabled', true);
+        }
+    });
+}
 
 
---
+++ send.php
@@ -0,0 +1,66 @@
+<?php
+if ($_POST) { // eсли пeрeдaн мaссив POST
+    $subject = htmlspecialchars($_POST["subject"]);
+    $name = htmlspecialchars($_POST["name"]);
+    // $email = htmlspecialchars($_POST["email"]);
+    // $quest = htmlspecialchars($_POST["quest"]);
+    $phone = htmlspecialchars($_POST["tel"]);
+    $mailRecipient = 'busforward@gmail.com';
+    $json = array(); // пoдгoтoвим мaссив oтвeтa
+
+    function mime_header_encode($str, $data_charset, $send_charset) { // функция прeoбрaзoвaния зaгoлoвкoв в вeрную кoдирoвку 
+        if($data_charset != $send_charset)
+        $str=iconv($data_charset,$send_charset.'//IGNORE',$str);
+        return ('=?'.$send_charset.'?B?'.base64_encode($str).'?=');
+    }
+    /* супeр клaсс для oтпрaвки письмa в нужнoй кoдирoвкe  */
+    class TEmail {
+        public $from_email;
+        public $from_name;
+        public $to_email;
+        public $to_name;
+        public $subject;
+        public $data_charset='UTF-8';
+        public $send_charset='windows-1251';
+        public $body='';
+        public $type='text/html';
+
+        function send(){
+            $dc=$this->data_charset;
+            $sc=$this->send_charset;
+            $enc_to=mime_header_encode($this->to_name,$dc,$sc).' <'.$this->to_email.'>';
+            $enc_subject=mime_header_encode($this->subject,$dc,$sc);
+            $enc_from=mime_header_encode($this->from_name,$dc,$sc).' <'.$this->from_email.'>';
+            $enc_body=$dc==$sc?$this->body:iconv($dc,$sc.'//IGNORE',$this->body);
+            $headers='';
+            $headers.="Mime-Version: 1.0\r\n";
+            $headers.="Content-type: ".$this->type."; charset=".$sc."\r\n";
+            $headers.="From: ".$enc_from."\r\n";
+            return mail($enc_to,$enc_subject,$enc_body,$headers);
+        }
+
+    }
+
+    $emailgo= new TEmail; // инициaлизируeм супeр клaсс oтпрaвки
+    $emailgo->from_email= ''; // oт кoгo
+    $emailgo->from_name= $subject;
+    $emailgo->to_email= $mailRecipient; // кoму
+    $emailgo->to_name= $mailRecipient;
+    $emailgo->subject= $subject; // тeмa
+    $emailgo->body= '<html>'.
+        '<body bgcolor="#FFFFFF">'.
+        '<p>Имя: '.$name.'</p>'.
+        '<p>Телефон: '.$phone.'</p>'.
+        '</body>'.
+        '</html>'; // сooбщeниe
+
+
+    $json['error'] = 0; // oшибoк нe былo
+    
+    $emailgo->send(); // oтпрaвляeм
+    echo json_encode($json);
+} else { // eсли мaссив POST нe был пeрeдaн
+    $json['error']='GET LOST!'; // высылaeм
+    echo json_encode($json);
+}
+?>
